bf9225dfbd6ba1ddfeb87e68951556b0c8d6a307
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-02-16 12:35:20 +0100

    feat: employer onboarding

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Onboard from "./pages/Onboard";
+import Onboard, { EmployerOnboard } from "./pages/Onboard";
 
 function App() {
   return (
@@ -12,6 +12,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Login />} />
           <Route path="/auth/onboarding" element={<Onboard />} />
+          <Route
+            path="/auth/employer/onboarding"
+            element={<EmployerOnboard />}
+          />
         </Routes>
       </Router>
     </>
